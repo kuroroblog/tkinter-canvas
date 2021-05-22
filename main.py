486cc87fbca22479f35a558d3de48f36b8becb2b
tocchimittsu <arimagedon21@gmail.com>
2021-05-22 09:03:19 +0900

    修正

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ class Application(tk.Frame):
         # Frameを親要素として、canvas Widgetを作成する。
         # width : 幅の大きさ
         # height : 高さの大きさ
-        self.canvas = tk.Canvas(width=1000, height=1000)
+        # background : 背景色
+        self.canvas = tk.Canvas(width=500, height=500, background="white")
 
         # Frameを親要素とした場合に、canvas Widgetをどのように配置するのか?
         # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
