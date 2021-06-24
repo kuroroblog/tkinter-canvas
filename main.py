5cc6d5a47d6a5879fab2cb863528eecd9c051e96
tocchimittsu <arimagedon21@gmail.com>
2021-06-25 08:52:59 +0900

    修正

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class Application(tk.Frame):
         # text : 文字列情報
         # fill : 文字列色
         # 色について : https://kuroro.blog/python/YcZ6Yh4PswqUzaQXwnG2/
-        # activefill : 文字列が選択される or 文字列の上にカーソルが移動する場合に、色を変更する設定
+        # activefill : 文字列が選択される or 文字列の上にマウスが移動する場合に、色を変更する設定
         # canvas Widgetのstate optionが'disabled' or create_textのstate optionが'disabled'の場合に、色を変更する設定
         # tag : 文字列をtesttestと'名前付け'を行う。
         canvas.create_text(250, 250, text="テストテスト", fill='black', activefill='red', disabledfill='blue', tag="testtest")
