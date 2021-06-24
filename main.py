65c86ead85881b6161e19e6f154808be29360430
tocchimittsu <arimagedon21@gmail.com>
2021-06-24 15:51:33 +0900

    修正

--- main.py
+++ main.py
@@ -17,12 +17,41 @@ class Application(tk.Frame):
         # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
         frame.pack()
 
+        ##########################################
+        # 1. canvas Widgetを定義する。              #
+        ##########################################
         # frame Widget(Frame)を親要素として、canvas Widgetを作成する。
         # width : 幅の設定
         # height : 高さの設定
         # background : 背景色の設定
         # 色について : https://kuroro.blog/python/YcZ6Yh4PswqUzaQXwnG2/
         canvas = tk.Canvas(frame, width=500, height=500, background="white")
+
+        ##########################################
+        # 2. canvas Widget内へ図形や画像を描画する。   #
+        ##########################################
+        # canvas Widget内へ文字列を描画する。(x座標, y座標)を'文字列の中心'として描画する。
+        # create_text(x座標, y座標, option1, option2, ...)
+        # 第一引数 : x座標
+        # 第二引数 : y座標
+        # 第三引数以降 : option
+        # text : 文字列情報
+        # fill : 文字列色
+        # 色について : https://kuroro.blog/python/YcZ6Yh4PswqUzaQXwnG2/
+        # activefill : 文字列が選択される or 文字列の上にカーソルが移動する場合に、色を変更する設定
+        # disabledfill : canvas Widgetのstate optionやcreate_textのstate optionが'disabled'の場合に、色を変更する設定
+        # tag : 文字列をtesttestと'名前付け'を行う。
+        canvas.create_text(250, 250, text="テストテスト", fill='black', activefill='red', disabledfill='blue', tag="testtest")
+
+        ##########################################
+        # 3. 図形や画像を操作する。                   #
+        ##########################################
+        # testtestとtag付け(名前付け)された図形や画像に対して操作を行う。
+        # 開始index(文字位置)2から終わりindex(文字位置)4に該当する文字列を、選択状態にする。
+        # select_from(tag名, 開始index(文字位置))
+        canvas.select_from("testtest", 2)
+        # select_to(tag名, 終わりindex(文字位置))
+        canvas.select_to("testtest", 4)
 
         # frame Widget(Frame)を親要素とした場合に、canvas Widgetをどのように配置するのか?
         # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
